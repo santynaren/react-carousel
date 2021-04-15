88d4e46c7797ec669c9840404c688b2b391c905e
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2021-04-15 22:15:59 +0530

    wip: added aboslute imports

--- src/Config/Helpers/strings.js
+++ src/Config/Helpers/strings.js
@@ -3,4 +3,6 @@ const carouselEndpointData = [
         title: "Random Images",
         images: ['https://picsum.photos/200','https://picsum.photos/200','https://picsum.photos/200','https://picsum.photos/200']
     }
-]
+];
+
+export {carouselEndpointData};
